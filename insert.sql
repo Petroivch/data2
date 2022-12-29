0ec4e7a44e16a9b7e47408528466baba3279cb6f
Petroivch <petr.tik03@gmail.com>
2022-12-29 11:55:27 +0300

    Initial commit

---
+++ insert.sql
@@ -0,0 +1,38 @@
+insert into customers(name, surname, age, phone_number)
+VALUES ('Alexey', 'D', 24, '11111111111');
+insert into customers(name, surname, age, phone_number)
+VALUES ('Ian', 'A', 20, '22222222222');
+insert into customers(name, surname, age, phone_number)
+VALUES ('ALEXEY', 'C', 12, '33333333333');
+insert into customers(name, surname, age, phone_number)
+VALUES ('aLeXey', 'R', 34, '44444444444');
+insert into customers(name, surname, age, phone_number)
+VALUES ('Maria', 'H', 32, '55555555555');
+insert into customers(name, surname, age, phone_number)
+VALUES ('Jhon', 'D', 25, '66666666666');
+insert into customers(name, surname, age, phone_number)
+VALUES ('Alex', 'Y', 19, '777777777777';
+insert into customers(name, surname, age, phone_number)
+VALUES ('Filipe', 'G', 29, '888888888888';
+insert into customers(name, surname, age, phone_number)
+VALUES ('George', 'O', 42, '99999999999');
+insert into customers(name, surname, age, phone_number)
+VALUES ('Eva', 'W', 16, '12345678901');
+insert into orders(product_name, amount)
+VALUES ('water', 3);
+insert into orders(product_name, amount)
+VALUES ('folder', 6);
+insert into orders(product_name, amount)
+VALUES (default, 8);
+insert into orders(product_name, amount)
+VALUES ('plant', 1);
+insert into orders(product_name, amount)
+VALUES ('book', 3);
+insert into orders(product_name, amount)
+VALUES ('paper bag', 1);
+insert into orders(product_name, amount)
+VALUES ('lightning', 2);
+insert into orders(product_name, amount)
+VALUES ('pen', 12);
+insert into orders(product_name, amount)
+VALUES ('hairbrush', 3);
